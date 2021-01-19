dff5317e9bf0f14704b4206934fc4a0f6e8d3814
Anna Chang <p227738930@hotmail.com>
2021-01-19 20:47:28 +0800

    Password pratice update

--- password.py
+++ password.py
@@ -1,18 +1,19 @@
+password = 'a123456'
 while True:
-	password = input('請輸入密碼:')
-	if password == 'a123456':
+	pwd = input('請輸入密碼:')
+	if pwd == password:
 		print('登入成功')
 		break
 	else:
 		print('密碼錯誤!還有2次機會')
-	password = input('請輸入密碼:')	
-	if password == 'a123456':
+	pwd = input('請輸入密碼:')	
+	if pwd == password:
 		print('登入成功')
 		break
 	else:
 		print('密碼錯誤!還有1次機會')
-	password = input('請輸入密碼:')	
-	if password == 'a123456':
+	pwd = input('請輸入密碼:')	
+	if pwd == password:
 		print('登入成功')
 		break
 	else:
