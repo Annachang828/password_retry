b7a9c011ce1c1dc3f32a8ac31e42b961d5c15b3c
Anna Chang <p227738930@hotmail.com>
2021-01-19 20:54:19 +0800

    basic version done

--- password.py
+++ password.py
@@ -1,22 +1,12 @@
 password = 'a123456'
+i = 3
 while True:
 	pwd = input('請輸入密碼:')
 	if pwd == password:
 		print('登入成功')
 		break
 	else:
-		print('密碼錯誤!還有2次機會')
-	pwd = input('請輸入密碼:')	
-	if pwd == password:
-		print('登入成功')
-		break
-	else:
-		print('密碼錯誤!還有1次機會')
-	pwd = input('請輸入密碼:')	
-	if pwd == password:
-		print('登入成功')
-		break
-	else:
-		print('密碼錯誤!還有0次機會,請洽服務人員')
-		break
-
+		i = i - 1
+		print('密碼錯誤!還有', i,'次機會')
+		if i == 0:
+			break
